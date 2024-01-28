410bdbaa16144e4efb8b190930bc481768c5f779
Xiayu Li <xli463@uottawa.ca>
2024-01-28 01:42:35 -0500

    Update backend
    -refactor
    -bug fix
    .....

--- backend/server.js
+++ backend/server.js
@@ -4,85 +4,95 @@ const crypto = require("crypto");
 const app = express();
 app.use(express.static("public"));
 
-app.post("/api/conversation", (req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache",
-  });
+let servers = [];
 
+function eventHandlers(req, res, next) {
+  let server = undefined;
   let uuid = undefined;
-  req.id == undefined ? (uuid = crypto.randomUUID()) : (uuid = req.id);
 
-  let counter = 0;
+  const { query } = req;
+  if (query.id == undefined) {
+    server = getNewServer(req, res, next);
+    servers.push(server);
+    uuid = servers;
+  } else {
+    server = servers.find((tmp) => tmp.id === query.id);
+    if (server == undefined) {
+      res.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache",
+      });
+      let error = {
+        type: "fail",
+        message: "Cannot find the conversation.",
+      };
+      res.write(JSON.stringify(error));
+    }
+  }
 
-  // Send a message every 5 seconds
-  setInterval(() => {
-    let message_id = crypto.randomUUID();
-    let author = { role: "system" };
-
-    //current default
-    let response_type = "message";
-    let data_type = "text";
-    let data = counter;
-
-    let message = {
-      id: message_id,
-      content_type: data_type,
-      content: data,
-      author: author,
-    };
-
-    let output = { type: response_type, id: uuid, message: message };
-
-    res.write("event: message\n");
-    res.write(JSON.stringify(output));
-    counter += 1;
-  }, 5000);
+  if (query.message != undefined) {
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+      Connection: "keep-alive",
+      "Cache-Control": "no-cache",
+    });
+    let message = newMessage(query.message);
+    server.message = message;
+    server.type = "message";
+    res.write(JSON.stringify(server));
+  } else {
+    let counter = 0;
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+      Connection: "keep-alive",
+      "Cache-Control": "no-cache",
+    });
+    while (counter < 10) {
+      let message = newMessage(counter);
+      server.message = message;
+      server.type = "message";
+      res.write(JSON.stringify(server));
+      counter += 1;
+    }
+  }
 
   // Close the connection when the client disconnects
   req.on("close", () => res.end("OK"));
-});
+}
 
-app.get("/api", (req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache",
-  });
+function getNewServer(req, res, next) {
+  let uuid = crypto.randomUUID();
+  let response_type = "Control";
+  let newServer = {
+    id: uuid,
+    type: response_type,
+    message: undefined,
+    control: undefined,
+  };
+  return newServer;
+}
 
-  let uuid = undefined;
-  req.id == undefined ? (uuid = crypto.randomUUID()) : (uuid = req.id);
+function newMessage(query) {
+  let solution = getSolution(query);
 
-  let counter = 0;
+  let message = {
+    id: crypto.randomUUID(),
+    content_type: solution.type,
+    content: solution.content,
+    author: { role: "system" },
+  };
+  return message;
+}
 
-  // Send a message every 5 seconds
-  setInterval(() => {
-    let message_id = crypto.randomUUID();
-    let author = { role: "system" };
+function getSolution(query) {
+  //Test
+  let solution = { content: query, type: "message" };
+  //TO DO: Implement the API call here
 
-    //current default
-    let response_type = "message";
-    let data_type = "text";
-    let data = counter;
+  return solution;
+}
 
-    let message = {
-      id: message_id,
-      content_type: data_type,
-      content: data,
-      author: author,
-    };
-
-    let output = { type: response_type, id: uuid, message: message };
-
-    res.write("event: message\n");
-    res.write(JSON.stringify(output));
-    res.write("\n\n");
-    counter += 1;
-  }, 5000);
-
-  // Close the connection when the client disconnects
-  req.on("close", () => res.end("OK"));
-});
+app.get("/api", eventHandlers);
 
 app.listen(3001, () => console.log("App listening: http://localhost:3001"));
